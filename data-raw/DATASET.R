b3cb7a4dd7d1c0a4c974dd4f1e22f21da772b2c8
Erickcufe <erickcufe94@gmail.com>
2021-05-26 13:39:48 -0500

    datos de programas sociales

---
+++ data-raw/DATASET.R
@@ -0,0 +1,24 @@
+## code to prepare `DATASET` dataset goes here
+
+ISeD_2019_2020 <- readr::read_csv("../../../Descargas/ISeD_2019-2020 csv/Resultados ISeD.csv")
+usethis::use_data(ISeD_2019_2020, overwrite = TRUE)
+
+
+Diccionario_ISeD_2019_2020 <- readr::read_csv("../../../Descargas/ISeD_2019-2020 csv/Diccionario de datos - ISeD .csv")
+usethis::use_data(Diccionario_ISeD_2019_2020, overwrite = TRUE)
+
+MSD_2013_2018 <- read("../../../Descargas/prog_valoracion_del_desempeno.csv")
+usethis::use_data(MSD_2013_2018, overwrite = TRUE)
+
+Diccionario_MSD <- readxl::read_xlsx("../../../Descargas/diccionario_msd.xlsx")
+Diccionario_MSD_names <- as.vector(Diccionario_MSD[4,c(1,2)])
+Diccionario_MSD <- Diccionario_MSD[-c(1,2,3,4),]
+colnames(Diccionario_MSD) <- c(Diccionario_MSD_names$`1.Título del documento`,
+                               Diccionario_MSD_names$`Modelo Sintético de Información de Desempeño`)
+usethis::use_data(Diccionario_MSD, overwrite = TRUE)
+
+
+# Poblaciones objetivo de los programas
+
+
+
